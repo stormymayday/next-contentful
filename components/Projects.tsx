e615ecbd7efa41c5f3adfa2cd92b064cd77693f3
stormymayday <stormymayday7@gmail.com>
2024-12-20 21:03:46 -0800

    Rendering cards via Projects.tsx

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,4 +1,4 @@
-import Project from "./Project";
+import Image from "next/image";
 
 type ProjectType = {
     id: string;
@@ -24,15 +24,33 @@ function Projects({ contentfulData, error }: ProjectsProps) {
     }
 
     return (
-        <section className="section projects">
-            <div className="section-center projects-center">
-                {contentfulData.map((project, index) => {
+        <section className="projects">
+            <div className="title">
+                <h2>projects</h2>
+                <div className="title-underline"></div>
+            </div>
+            <div className="projects-center">
+                {contentfulData.map((project) => {
+                    const { id, title, image, liveUrl } = project;
                     return (
-                        <Project
-                            key={project.id}
-                            index={index}
-                            project={project}
-                        />
+                        <a
+                            key={id}
+                            href={liveUrl}
+                            target="_blank"
+                            rel="noreferrer"
+                            className="project"
+                        >
+                            {image && (
+                                <Image
+                                    src={`https:${image}`}
+                                    alt={title}
+                                    height={500}
+                                    width={500}
+                                    className="img"
+                                />
+                            )}
+                            <h5>{title}</h5>
+                        </a>
                     );
                 })}
             </div>
